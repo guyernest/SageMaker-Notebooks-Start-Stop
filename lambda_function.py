6510578f9a15e098dbd80729f694d8b961111e35
guyernest <ec2-user@ip-172-16-87-167.eu-west-1.compute.internal>
2018-08-24 21:04:10 +0000

    Adding Terraform

---
+++ lambda_function.py
@@ -0,0 +1,30 @@
+import boto3
+import logging
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+client = boto3.client('sagemaker')
+
+def lambda_handler(event, context):
+    
+    candidates = client.list_notebook_instances()
+    
+    for candidate in candidates['NotebookInstances']:
+        candidateName = candidate['NotebookInstanceName']
+        logger.info('instance {0} is in Service'.format(candidateName))
+        instanceArn = candidate['NotebookInstanceArn']
+        instanceTags = client.list_tags(ResourceArn=instanceArn)
+        for tag in instanceTags['Tags']:
+            if (tag['Key'] == 'InDuty' and tag['Value'] =='Yes'):
+                logger.info('instance {0} is in duty'.format(candidateName))
+                if (event['event'] == "On"):
+                    response = client.start_notebook_instance(NotebookInstanceName=candidateName)
+                    logger.info('instance {0} was started'.format(candidateName))
+                elif (event['event'] == "Off"):
+                    response = client.stop_notebook_instance(NotebookInstanceName=candidateName)
+                    logger.info('instance {0} was stopped'.format(candidateName))
+            else:
+                logger.info('Not in duty'.format(candidateName))
+
+    return "Done"
